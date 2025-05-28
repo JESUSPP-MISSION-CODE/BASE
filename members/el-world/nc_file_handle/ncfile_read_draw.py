4a4b98acfb3b171916a778fdaff200dd6f9d798b
Sweetsalt25 <128217746+Sweetsalt25@users.noreply.github.com>
2025-05-28 14:59:09 +0900

    Update ncfile_read_draw.py

--- members/el-world/nc_file_handle/ncfile_read_draw.py
+++ members/el-world/nc_file_handle/ncfile_read_draw.py
@@ -9,14 +9,29 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 fn = 'MERRA2_400.tavg1_2d_rad_Nx.20250501.nc4'
-f = nc.Dataset(fn,'r')
-lat = f.variables['lat'][:]
-lon = f.variables['lon'][:]
-data = f.variables['LWGEM'][:]
+fn_land = 'lsmask.oisst.nc'
+
+f = nc.Dataset(fn, 'r')
+land_data = nc.Dataset(fn_land, 'r')
+
+lon = land_data.variables['lon'][:]
+lat = land_data.variables['lat'][:]
+land = land_data.variables['lsmask'][:]
+fig=plt.figure(figsize=(16,9))
+ax = fig.add_subplot(111)
+
+cont=ax.contourf(lon-180,lat,land[0,:,:],colors='#F8ECD2')
+conline=ax.contour(lon-180, lat, land[0,:,:], colors='k',linewidths=0.2,alpha=0.8)
+
+
+lat_2 = f.variables['lat'][:]
+lon_2 = f.variables['lon'][:]
+data_2 = f.variables['LWGEM'][:]
+
 
 # Create plot
-fig, ax = plt.subplots()  #Create a single subplot
-contour = ax.contourf(lon, lat, data[0,:,:], cmap='viridis') #Assuming data is 3D (time, lat, lon)
+# fig, ax = plt.subplots(111)  #Create a single subplot
+contour = ax.contourf(lon_2, lat_2, data_2[0,:,:], cmap='viridis') #Assuming data is 3D (time, lat, lon)
 
 # Add labels and title
 ax.set_xlabel('Longitude')
